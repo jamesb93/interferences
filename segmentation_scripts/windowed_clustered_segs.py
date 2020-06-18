ac128cc5dcf248f3e8925e269d6f33018608ee7a
James Bradbury <jamesbradbury93@gmail.com>
2020-06-18 10:34:47 +0100

    exhaustive scripting
    Former-commit-id: 7c2429f9bcca33c1dc4d6a299e59d38f8afcba0a

--- segmentation_scripts/windowed_clustered_segs.py
+++ segmentation_scripts/windowed_clustered_segs.py
@@ -7,13 +7,10 @@ from pathlib import Path
 from uuid import uuid4
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
-from datetime import date
+from datetime import datetime
 
-COMPONENTS = 4  # UMAP Components
-NEIGHBOURS = 7  # UMAP neighbours
-MINDIST = 0.1  # UMAP minimum distance
 THRESHOLD = 0.47
-WINDOWSIZE = 10
+WINDOWSIZE = 25
 HOPSIZE = 1
 
 media = Path("../reaper/source/media/")
@@ -37,88 +34,103 @@ standardise = StandardScaler()
 original_slices = list(slices) # make a templated copy
 tracks = {}
 
-for nclusters in range(2, WINDOWSIZE):
-    model = AgglomerativeClustering(n_clusters=nclusters)
-    count = 0
-    slices = original_slices[:] # recopy the original so we start fresh
-    while (count + WINDOWSIZE) <= len(slices):
-        indices = slices[count:count+WINDOWSIZE] #create a section of the indices in question
-        data = []
-        for i, (start, end) in enumerate(zip(indices, indices[1:])):
+pos = 0
+for i, (start, end) in enumerate(zip(original_slices, original_slices[1:])):
+    start = (start / 44100)
+    end = (end / 44100)
 
-            mfcc = fluid.mfcc(source, 
-                fftsettings = [2048, -1, -1],
-                startframe = start,
-                numframes = end-start)
+    item = {
+        "file": source.resolve(),
+        "length": end - start,
+        "start": start,
+        "position": pos
+    }
+    pos += end-start
 
-            stats = get_buffer(
-                fluid.stats(mfcc,
-                    numderivs = 1
-                ), "numpy")
+    if "original" in tracks:
+        tracks["original"].append(item)
+    else:
+        tracks["original"] = [item]
 
-            data.append(stats.flatten())
 
-        data = standardise.fit_transform(data)
+for window in range(3, WINDOWSIZE):
+    print(f"At window size: {window}")
+    for nclusters in range(2, window):
+        print(f"At cluster size: {nclusters}")
+        model = AgglomerativeClustering(n_clusters=nclusters)
+        count = 0
+        slices = list(original_slices) # recopy the original so we start fresh
 
-        # might not be necessary to reduce as the dimensions are already quite low
-        # redux = UMAP(n_components=COMPONENTS, n_neighbors=NEIGHBOURS, min_dist=MINDIST, random_state=42).fit_transform(data)
+        while (count + window) <= len(slices):
+            indices = slices[count:count+window] #create a section of the indices in question
+            data = []
+            for i, (start, end) in enumerate(zip(indices, indices[1:])):
 
-        cluster = model.fit(data)
-        print(f"num slices {len(slices)}")
+                mfcc = fluid.mfcc(source, 
+                    fftsettings = [2048, -1, -1],
+                    startframe = start,
+                    numframes = end-start)
+
+                stats = get_buffer(
+                    fluid.stats(mfcc,
+                        numderivs = 1
+                    ), "numpy")
+
+                data.append(stats.flatten())
+
+            data = standardise.fit_transform(data)
+
+            # might not be necessary to reduce as the dimensions are already quite low
+            # redux = UMAP(n_components=COMPONENTS, n_neighbors=NEIGHBOURS, min_dist=MINDIST, random_state=42).fit_transform(data)
+
+            cluster = model.fit(data)
+            
+            cur = -2
+            for j, c in enumerate(cluster.labels_):
+                prev = cur
+                cur = c
+                if cur == prev:
+                    try:
+                        slices.pop(j + count)
+                    except IndexError:
+                        print(f"Error at {j}")
+                        print(f"Count {count}")
+                        print(len(slices))
+
+            count += 1
         
-        cur = -2
-        for j, c in enumerate(cluster.labels_):
-            prev = cur
-            cur = c
-            if cur == prev:
-                try:
-                    slices.pop(j + count)
-                except IndexError:
-                    print(f"Error at {j}")
-                    print(f"Count {count}")
-                    print(len(slices))
+        pos = 0
+        track_id = f"{nclusters}-{window}"
+        for i, (start, end) in enumerate(zip(slices, slices[1:])):
+            start = (start / 44100)
+            end = (end / 44100)
 
-        count += 1
+            item = {
+                "file": source.resolve(),
+                "length": end - start,
+                "start": start,
+                "position": pos
+            }
+            pos += end-start
 
+            if track_id in tracks:
+                tracks[track_id].append(item)
+            else:
+                tracks[track_id] = [item]
 
-    # Create reaper files to look at the results
-    
-    pos = 0
-    track_id = str(nclusters)
-    for i, (start, end) in enumerate(zip(slices, slices[1:])):
-        start = (start / 44100)
-        end = (end / 44100)
+    # make the necessary folders
+    now = str(datetime.now()).replace(':', "-")
+    session = Path(now)
+    if not session.exists(): session.mkdir()
+    reaper_session = session / "session.rpp"
 
-        item = {
-            "file": source.resolve(),
-            "length": end - start,
-            "start": start,
-            "position": pos
-        }
-        pos += end-start
-
-        if nclusters in tracks:
-            tracks[nclusters].append(item)
-        else:
-            tracks[nclusters] = [item]
-
-# make the necessary folders
-today = date.today()
-now = today.strftime("%d-%m-%Y")
-session_id = str(uuid4().hex)[:5]
-session = Path(f"{now}-{session_id}")
-if not session.exists(): session.mkdir()
-reaper_session = session / "session.rpp"
-
-# create a dictionary of metadata
-metadata = {
-    "components" : COMPONENTS,
-    "mindist" : MINDIST,
-    "neighbours" : NEIGHBOURS,
-    "threshold" : THRESHOLD,
-    "note" : "Windowed clustering",
-    "window" : WINDOWSIZE
-}
+    # create a dictionary of metadata
+    metadata = {
+        "clusters" : nclusters,
+        "threshold" : THRESHOLD,
+        "note" : "Windowed clustering",
+        "window" : window
+    }
 
 print('Generating REAPER file')
 # now create the reaper project
