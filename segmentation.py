2d79e9c94efce44842a46aae6a8a3dda1a372cad
James Bradbury <jamesbradbury93@gmail.com>
2020-07-07 11:32:52 +0100

    update segmentation script
    Former-commit-id: 192745584888c1aee0a750ea0f287cf9170fc44d

--- segmentation.py
+++ segmentation.py
@@ -1,3 +1,8 @@
+# Description
+"""
+Segment using clustered segmentation approach and create an audio file per segment
+"""
+
 from ftis.analyser import (
     FluidNoveltyslice,
     ClusteredSegmentation,
@@ -6,7 +11,7 @@ from ftis.analyser import (
 from ftis.process import FTISProcess as Chain
 
 src = "reaper/highgain_source/bounces"
-folder = "segments2"
+folder = "outputs/segments"
 
 process = Chain(
     source=src, 
@@ -14,9 +19,10 @@ process = Chain(
 )
 
 process.add(
-    FluidNoveltyslice(threshold=0.4,minslicelength=4096, feature=0),
-    ClusteredSegmentation(numclusters=3, windowsize=5),
+    FluidNoveltyslice(threshold=0.1,minslicelength=4096, feature=0, cache=True),
+    ClusteredSegmentation(numclusters=3, windowsize=5, cache=True),
     ExplodeAudio()
 )
 
-process.run()
+if __name__ == "__main__":
+    process.run()
