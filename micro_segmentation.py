0a36348a755c0474ac23263db0cc7cac2c318355
James Bradbury <jamesbradbury93@gmail.com>
2020-07-13 16:46:52 +0100

    write out segmentation to reaper file
    Former-commit-id: ac5913eb8bfa7b64acb4160d774f4b40765c1206

--- micro_segmentation.py
+++ micro_segmentation.py
@@ -25,3 +25,34 @@ process.add(
 
 if __name__ == "__main__":
     process.run()
+    # Write out the clustered segemnts to a REAPER file
+    tracks = {}
+    for audio_src, slices in zip(cluster_segmentation.output.keys(), cluster_segmentation.output.values()):
+        f = Path(audio_src).resolve()
+        track_id = str(f.name)
+        pos = 0
+        for i, (start, end) in enumerate(zip(slices, slices[1:])):
+            start /= 44100
+            end /= 44100
+
+            item = {
+                "file": f,
+                "length": end - start,
+                "start": start,
+                "position": pos
+            }
+            pos += end-start
+
+            if track_id in tracks:
+                tracks[track_id].append(item)
+            else:
+                tracks[track_id] = [item]
+
+        # make the necessary folders
+        reaper_session = Path(folder) / "2_VisaluseSlices.rpp"
+
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(['./RPRTemplates']))
+        template = env.get_template("SegmentationTemplate.rprtemplate")
+
+        with open(reaper_session, "w") as f:
+            f.write(template.render(tracks=tracks, metadata={"none" : "none"}))
