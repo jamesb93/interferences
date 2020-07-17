979f5a180429bdeb7487ee5611cc5242bca794c9
James Bradbury <jamesbradbury93@gmail.com>
2020-07-17 13:36:45 +0100

    clustering on the micro_segmentation
    Former-commit-id: ee89523805f69073c2dddb282f7d871320e44ea9

---
+++ micro_clustering.py
@@ -0,0 +1,33 @@
+"""
+Analyse each segment in the 'gestural' pool and cluster it
+"""
+
+from ftis.analyser import (
+    FluidMFCC, 
+    HDBSCluster, 
+    Stats, 
+    UmapDR, 
+    Standardise,
+    Normalise
+)
+from ftis.process import FTISProcess as Chain
+
+src = "outputs/micro_segmentation/2_ExplodeAudio"
+folder = "outputs/micro_clustering"
+
+process = Chain(
+    source=src, 
+    folder=folder
+)
+
+process.add(
+    FluidMFCC(cache=True),
+    Stats(numderivs=1, flatten=True, cache=False),
+    Standardise(cache=False),
+    Normalise(cache=False),
+    UmapDR(components=6),
+    HDBSCluster(minclustersize=10)
+)
+
+if __name__ == "__main__":
+    process.run()
