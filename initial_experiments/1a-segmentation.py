db73c9a2e65e6cbf0fdd9d49c153d1542d3c2654
James Bradbury <jamesbradbury93@gmail.com>
2021-01-17 17:15:42 +0000

    imports

--- initial_experiments/1a-segmentation.py
+++ initial_experiments/1a-segmentation.py
@@ -3,64 +3,62 @@
 Segment using clustered segmentation approach and create an audio file per segment
 """
 
-import jinja2
-from ftis.analyser import (
-    FluidNoveltyslice,
-    ClusteredSegmentation,
-    ExplodeAudio,
-    CollapseAudio
-)
-from ftis.process import FTISProcess as Chain
-from pathlib import Path
+from ftis.analyser.flucoma import Noveltyslice
+from ftis.analyser.meta import ClusteredSegmentation
+from ftis.analyser.audio import ExplodeAudio, CollapseAudio
 from ftis.common.conversion import samps2ms
+from ftis.world import World # a ftis 'world'
+from ftis.corpus import Corpus # a corpus object
+from pathlib import Path
 
+src = Corpus("../reaper/highgain_source/bounces")
+# folder = "outputs/segments"
+folder = "../dump/segments"
 
-src = "reaper/highgain_source/bounces"
-folder = "outputs/segments"
+world = World(sink=folder)
 
-process = Chain(
-    source=src, 
-    folder=folder
+slicer = Noveltyslice(
+    threshold=0.1, 
+    minslicelength=4096, 
+    feature=0, 
+    cache=1
 )
 
 cs = ClusteredSegmentation(numclusters=3, windowsize=5)
 
-process.add(
-    FluidNoveltyslice(threshold=0.1,minslicelength=4096, feature=0),
-    cs,
-    ExplodeAudio()
-)
+src >> slicer >> cs >> ExplodeAudio()
+world.build(src)
 
 if __name__ == "__main__":
-    process.run()
+    world.run()
 
-    tracks = {}
-    for audio_src, slices in zip(cs.output.keys(), cs.output.values()):
-        f = Path(audio_src).resolve()
-        track_id = str(f.name)
-        pos = 0
-        for i, (start, end) in enumerate(zip(slices, slices[1:])):
-            start /= 44100
-            end /= 44100
+    # tracks = {}
+    # for audio_src, slices in zip(cs.output.keys(), cs.output.values()):
+    #     f = Path(audio_src).resolve()
+    #     track_id = str(f.name)
+    #     pos = 0
+    #     for i, (start, end) in enumerate(zip(slices, slices[1:])):
+    #         start /= 44100
+    #         end /= 44100
 
-            item = {
-                "file": f,
-                "length": end - start,
-                "start": start,
-                "position": pos
-            }
-            pos += end-start
+    #         item = {
+    #             "file": f,
+    #             "length": end - start,
+    #             "start": start,
+    #             "position": pos
+    #         }
+    #         pos += end-start
 
-            if track_id in tracks:
-                tracks[track_id].append(item)
-            else:
-                tracks[track_id] = [item]
+    #         if track_id in tracks:
+    #             tracks[track_id].append(item)
+    #         else:
+    #             tracks[track_id] = [item]
 
-        # make the necessary folders
-        reaper_session = Path(folder) / "2_VisaluseSlices.rpp"
+    #     # make the necessary folders
+    #     reaper_session = Path(folder) / "2_VisaluseSlices.rpp"
 
-        env = jinja2.Environment(loader=jinja2.FileSystemLoader(['./RPRTemplates']))
-        template = env.get_template("SegmentationTemplate.rprtemplate")
+    #     env = jinja2.Environment(loader=jinja2.FileSystemLoader(['./RPRTemplates']))
+    #     template = env.get_template("SegmentationTemplate.rprtemplate")
 
-        with open(reaper_session, "w") as f:
-            f.write(template.render(tracks=tracks, metadata={"none" : "none"}))
+    #     with open(reaper_session, "w") as f:
+    #         f.write(template.render(tracks=tracks, metadata={"none" : "none"}))
