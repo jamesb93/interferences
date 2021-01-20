f18f98865270f117b840bcf31153a20a49680142
James Bradbury <jamesbradbury93@gmail.com>
2021-01-20 11:45:44 +0000

    rework stage 3

---
+++ initial_experiments/3-micro_segmentation.py
@@ -0,0 +1,84 @@
+# Description
+"""
+A heavy sub-segmentation of the classified samples from the source.
+The idea is to return short transients and micro gestures, wittling down to the micro level as much as is perceptually reasonable.
+"""
+from ftis.world import World
+from ftis.corpus import Corpus
+from ftis.analyser.meta import ClusteredSegmentation
+from ftis.analyser.flucoma import Onsetslice
+from ftis.analyser.audio import ExplodeAudio
+from ftis.analyser.dr import UMAP
+from ftis.common.conversion import samps2ms
+from pathlib import Path
+from reathon import nodes as reaper
+
+src = Corpus("../dump/classification/4_Split/1") # the suffix number might change, needs to be on the low activity ones
+out = "../dump/micro_segmentation"
+world = World(sink=out)
+
+
+initial_segmentation = Onsetslice(threshold=0.3, cache=True)
+cluster_segmentation = ClusteredSegmentation(cache=True)
+
+src >> initial_segmentation >> cluster_segmentation >> ExplodeAudio()
+
+world.build(src)
+
+if __name__ == "__main__":
+    world.run()
+    # Write out the clustered segemnts to a REAPER file
+    session = reaper.Project()
+
+    # Construct tracks  
+    for audio_src, slices in cluster_segmentation.output.items():
+        p = Path(audio_src)
+        source = reaper.Source(file = p.resolve())
+        track = reaper.Track(name=p.stem)
+        pos = 0
+        for i, (start, end) in enumerate(zip(slices, slices[1:])):
+            start /= 44100
+            end /= 44100
+                
+            track.add(
+                reaper.Item(
+                    source,
+                    position = float(pos),
+                    length = float(end-start)
+                )
+            )
+            
+            pos += end-start
+        session.add(track)
+    session.write(Path(out) / "2_VisaluseSlices.rpp")
+
+    # tracks = {}
+    # for audio_src, slices in cluster_segmentation.output.items():
+    #     f = Path(audio_src).resolve()
+    #     track_id = str(f.name)
+    #     pos = 0
+    #     for i, (start, end) in enumerate(zip(slices, slices[1:])):
+    #         start /= 44100
+    #         end /= 44100
+
+    #         item = {
+    #             "file": f,
+    #             "length": end - start,
+    #             "start": start,
+    #             "position": pos
+    #         }
+    #         pos += end-start
+
+    #         if track_id in tracks:
+    #             tracks[track_id].append(item)
+    #         else:
+    #             tracks[track_id] = [item]
+
+    #     # make the necessary folders
+    #     reaper_session = Path(folder) / "2_VisaluseSlices.rpp"
+
+    #     env = jinja2.Environment(loader=jinja2.FileSystemLoader(['./RPRTemplates']))
+    #     template = env.get_template("SegmentationTemplate.rprtemplate")
+
+    #     with open(reaper_session, "w") as f:
+    #         f.write(template.render(tracks=tracks, metadata={"none" : "none"}))
